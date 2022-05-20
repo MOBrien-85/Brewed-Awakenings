04cda9b9904d9951387949c74d17d97aa4607ce3
Michael O'Brien <michael.obrien45@gmail.com>
2022-05-20 16:16:06 -0500

    adding all files

---
+++ scripts/Products.js
@@ -0,0 +1,39 @@
+import { getProducts } from "./database.js"
+
+// add eventlistener - clickevent. popup showing price of the product
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+
+
+                    window.alert(`${product.name} costs $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
+
+
+
+
+
+const products = getProducts()
+
+export const Products = () => {
+    let html = "<ul>"
+
+    for (const product of products) {
+        html += `<li id="product--${product.id}">${product.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
